f127772ebd3da39a27695645d3bf61fff3664c2b
O H <olle@unremarkablegarden.com>
2018-08-16 20:11:01 +0200

    honk

---
+++ vue-cli-graphql-graphcool/src/components/ProjectEdit.vue
@@ -0,0 +1,162 @@
+<template lang="pug">
+  #projectedit
+    el-button(@click='dialogVisible = true', icon='el-icon-edit', size='mini').add Projekt bearbeiten
+
+    el-dialog(title='Projekt bearbeiten', :visible.sync='dialogVisible', width='800', :close-on-click-modal='false')
+
+      el-form(:model='form', ref='form', :rules='rules', label-width='110px', v-loading='loading', status-icon, @keyup.enter.native="submitForm('form')").form
+
+        el-form-item(label='Title', prop='name')
+          el-input(type='text', v-model='form.name', placeholder="Project title")
+
+        el-form-item(label='Description', prop='description')
+          el-input(type="textarea", :autosize="{ minRows: 4, maxRows: 8 }", placeholder="Project description", v-model="form.description")
+
+        el-row
+          el-col(:span='8')
+            el-form-item(label='Selbstlernbox', prop='selfLearn')
+              el-switch(v-model='form.selfLearn')
+          el-col(:span='10')
+            el-form-item(label='Published', prop='published')
+              el-switch(v-model='form.isPublished')
+
+        el-form-item(label='Level', prop='level')
+          el-radio-group(v-model='form.level', size='small')
+            el-radio(v-for='level in levelsOptions', :key='level', :label='level', border) {{ level }}
+
+        el-form-item
+          el-button(type='primary', @click="submitForm('form')", :loading='loading', icon='el-icon-check')
+            span(v-if='loading') Speichern...
+            span(v-else) Speichern
+</template>
+
+<script>
+import slug from 'slug'
+import PROJECT_EDIT from '../graphql/ProjectEdit.gql'
+
+export default {
+  name: 'projectedit',
+  props: {
+    projectData: {
+      type: Object,
+      default: function () {
+        return {
+          id: '',
+          name: '',
+          description: '',
+          selfLearn: false,
+          isPublished: false
+        }
+      }
+    }
+  },
+  data () {
+    return {
+      levelsOptions: ['GK', 'BK', 'AK', 'AK1', 'AK2'],
+      form: null,
+      rules: {
+        name: [
+          { required: true, message: 'Please input a title', trigger: 'change' }
+        ],
+        description: [
+          { required: true, message: 'Please input a description', trigger: 'change' }
+        ],
+        level: [
+          { required: true, message: 'Select a level', trigger: ['blur', 'change'] }
+        ]
+      },
+      loading: false,
+      error: false,
+      dialogVisible: false,
+    }
+  },
+  created() {
+    this.form = JSON.parse(JSON.stringify(this.projectData))
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid && localStorage.getItem('userId')) {
+          this.apolloMutation()
+        } else {
+          this.$message({
+            type: 'error',
+            message: 'Form error'
+          })
+          return false
+        }
+      });
+    },
+
+    apolloMutation () {
+      this.loading = true
+      const formCopy = Object.assign({}, this.form)
+      this.$refs.form.resetFields()
+
+      this.$apollo.mutate({
+        mutation: PROJECT_EDIT,
+        variables: {
+          id: formCopy.id,
+          name: formCopy.name,
+          slug: slug(formCopy.name, {lower: true}),
+          description: formCopy.description,
+          selfLearn: formCopy.selfLearn,
+          isPublished: formCopy.isPublished,
+          level: formCopy.level
+        },
+        // updateQueries: {
+        //   mySubjects: (prev, { mutationResult }) => {
+        //     console.log('--updateQueries--')
+        //     console.log(prev)
+        //     console.log(mutationResult)
+        //     return {
+        //       mySubjects: [mutationResult.data.createProject, ...prev.mySubjects],
+        //     }
+        //   }
+        // }
+      })
+      .then((data) => {
+        console.log('--projectEdit then()--')
+        console.log(data)
+
+        this.$apolloProvider.defaultClient.reFetchObservableQueries()
+
+        this.loading = false
+        this.dialogVisible = false
+      })
+      .catch((error) => {
+        console.error(error)
+        this.$message({
+          type: 'error',
+          message: error.message
+        })
+        this.form = formCopy
+        this.loading = false
+      })
+    },
+
+    clearObjProps (obj) {
+      Object.keys(obj).forEach((k) => { obj[k] = null })
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  // .subjects
+  //   .el-card__body
+  //     padding-top: 0 !important
+  // .subject
+  //   &:first-child
+  //     .el-collapse
+  //       border-top: 0 !important
+  //   &:last-child
+  //     .el-collapse
+  //       border-bottom: 0 !important
+</style>
+
+<style lang="sass" scoped>
+  // .add
+  //   margin-top: 1em
+
+</style>
