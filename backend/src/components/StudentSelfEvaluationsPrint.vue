e833fca4f9bbea4dd3aead7b34b414f7852a2ca7
marijndegenaar <marijndegenaar@gmail.com>
2018-10-08 19:38:49 +0200

    added depedencies

---
+++ backend/src/components/StudentSelfEvaluationsPrint.vue
@@ -0,0 +1,397 @@
+<template lang="pug">
+  #selfEvaluations
+    .title Kompetenzen
+    .evals(v-if='selfEvaluations')
+      div(:loading='loading', stretch)
+        div(v-for='(level, index) in subjectLevels', :key='level', :label="level", :class='level')
+          template(slot='label')
+            | &nbsp;&nbsp;&nbsp;&nbsp;
+            | {{ level }}
+            | &nbsp;&nbsp;&nbsp;&nbsp;
+          ul
+            li.eval(v-for='e in competences', v-if='competencesLoaded')
+              strong {{ e.name }}
+              span &nbsp;— {{ getEvalValue(e.id, level) }}/10
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  name: 'studentselfevaluationsPrint',
+  props: ['subjectId', 'studentId'],
+  data () {
+    return {
+      loading: true,
+      competencesLoaded: false,
+      evalsByLevel: [
+        {
+          level: 'BK',
+          data: []
+        },
+        {
+          level: 'GK',
+          data: []
+        },
+        {
+          level: 'AK',
+          data: []
+        },
+        {
+          level: 'AK1',
+          data: []
+        },
+        {
+          level: 'AK2',
+          data: []
+        }
+      ]
+    }
+  },
+
+  methods: {
+    getEvalValue (competenceId, level) {
+      let returnVal
+      // subject and student set in query
+      // filter by comp ID and level
+      const data = this.selfEvaluations.filter(o => o.level === level)
+      if (data) {
+        const comp = data.find(o => o.competence.id === competenceId)
+        if (comp) { returnVal = comp.value}
+        else { returnVal = 0 }
+      } else {
+        returnVal = 0
+      }
+      return returnVal
+    },
+
+    setEvalValue (competenceId, level, val) {
+      if (this.hasEvalValue(competenceId, level)) {
+        this.updateEvalValue(competenceId, level, val)
+      } else {
+        this.createEvalValue(competenceId, level, val)
+      }
+    },
+
+    updateEvalValue (competenceId, level, val) {
+      const data = this.selfEvaluations.filter(o => o.level === level)
+      const comp = data.find(o => o.competence.id === competenceId)
+
+      this.$apollo.mutate({
+        mutation: gql`mutation ($id: ID!, $value: Int!) {
+          updateEvaluation(id: $id, value: $value) {
+            id
+            value
+          }
+        }`,
+        variables: {
+          id: comp.id,
+          value: val
+        },
+      }).then((data) => {
+        console.log(data)
+        this.msg('success', 'Evaluation saved')
+      }).catch((error) => {
+        console.error(error)
+        this.msg('error', error)
+      }).then(() => {
+        this.loading = false
+      })
+    },
+
+    createEvalValue (competenceId, level, val) {
+      this.$apollo.mutate({
+        mutation: gql`mutation ($value: Int!, $studentId: ID!, $competenceId: ID!, $subjectId: ID!, $level: Levels!) {
+          createEvaluation(value: $value, studentId: $studentId, competenceId: $competenceId, subjectId: $subjectId, level: $level) {
+            id
+            value
+          }
+        }`,
+        variables: {
+          value: val,
+          studentId: this.studentId,
+          competenceId: competenceId,
+          subjectId: this.subjectId,
+          level: level
+        },
+      }).then((data) => {
+        this.$apolloProvider.defaultClient.reFetchObservableQueries()
+        console.log(data)
+        this.msg('success', 'Evaluation saved')
+      }).catch((error) => {
+        console.error(error)
+        this.msg('error', error)
+      }).then(() => {
+        this.loading = false
+      })
+    },
+
+    hasEvalValue (competenceId, level) {
+      let returnVal
+      const data = this.selfEvaluations.filter(o => o.level === level)
+      if (data) {
+        const comp = data.find(o => o.competence.id === competenceId)
+        if (comp) { returnVal = true }
+        else { returnVal = false }
+      } else {
+        returnVal = false
+      }
+      return returnVal
+    },
+
+    msg (type, message) {
+      this.$message({
+         type: type,
+         message: message
+      })
+    }
+  },
+
+  apollo: {
+    subjectLevels: {
+      query: gql`query ($subjectId: ID!) {
+        Subject(id: $subjectId) {
+          levels
+        }
+      }`,
+      variables () {
+        return {
+          subjectId: this.subjectId,
+        }
+      },
+      update (result) {
+        // console.log('self evals subjectLevels result')
+        // console.log(result.Subject.levels)
+        return this.$sortLevels(result.Subject.levels)
+      },
+    },
+
+    competences: {
+      query: gql`query ($subjectId: ID!) {
+        Subject(id: $subjectId) {
+          competences {
+            id
+            slug
+            name
+          }
+        }
+      }`,
+      variables () {
+        return {
+          subjectId: this.subjectId,
+        }
+      },
+      update (result) {
+        // console.log('self evals competences');
+        // console.log(result.Subject.competences);
+        return result.Subject.competences
+      },
+      result (res) {
+        this.competencesLoaded = true
+      }
+    },
+
+    selfEvaluations: {
+      query: gql`query ($subjectId: ID!, $studentId: ID!) {
+        User(id: $studentId) {
+          selfEvaluations(filter: {
+            subject: {
+              id: $subjectId
+            }
+          }) {
+            id
+            value
+            level
+            competence {
+              name
+              id
+            }
+            subject {
+              id
+              name
+            }
+            student {
+              id
+              username
+            }
+          }
+        }
+      }`,
+      variables () {
+        return {
+          subjectId: this.subjectId,
+          studentId: this.studentId
+        }
+      },
+      update (result) {
+        const evaluations = result.User.selfEvaluations
+        // clear it
+        this.evalsByLevel.forEach(e => {
+          e.data = []
+        })
+        // set it
+        evaluations.forEach(e => {
+          this.evalsByLevel.find(o => o.level === e.level).data.push(e)
+        })
+        this.loading = false
+        // console.log('self evals selfEvals');
+        // console.log(evaluations)
+        return evaluations
+      },
+      result (result) {
+        this.loading = false
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+  $c1: #FCB714
+  $c2: #EC2126
+  $c3: #653393
+  $c4: #48A7D6
+  $c5: #0B643A
+
+  .title
+    font-weight: bold
+    margin-bottom: 1em
+  .circles
+    display: flex
+    justify-content: space-between
+    margin: 0.5em 0
+  .circle
+    height: 24px
+    width: 24px
+    border-radius: 20px
+    border: 2px white solid
+    opacity: 0.7
+    cursor: pointer
+    &.active
+      border: 1px black solid
+      opacity: 1
+      cursor: default
+    .inner
+      height: 22px
+      width: 22px
+      border: 2px white solid
+      border-radius: 20px
+
+  .BK .circle:nth-child(1) .inner
+    background: #FCB814
+  .BK .circle:nth-child(2) .inner
+    background: #F7A318
+  .BK .circle:nth-child(3) .inner
+    background: #F38F1B
+  .BK .circle:nth-child(4) .inner
+    background: #EF7D1F
+  .BK .circle:nth-child(5) .inner
+    background: #ED7122
+  .BK .circle:nth-child(6) .inner
+    background: #EB6624
+  .BK .circle:nth-child(7) .inner
+    background: #E75328
+  .BK .circle:nth-child(8) .inner
+    background: #E4442B
+  .BK .circle:nth-child(9) .inner
+    background: #E0332E
+  .BK .circle:nth-child(10) .inner
+    background: #DC2031
+
+
+  .GK .circle:nth-child(1) .inner
+  	background: #ED1D25
+  .GK .circle:nth-child(2) .inner
+  	background: #DC1F32
+  .GK .circle:nth-child(3) .inner
+  	background: #CF223E
+  .GK .circle:nth-child(4) .inner
+  	background: #BE244B
+  .GK .circle:nth-child(5) .inner
+  	background: #B22654
+  .GK .circle:nth-child(6) .inner
+  	background: #A22961
+  .GK .circle:nth-child(7) .inner
+  	background: #962B6C
+  .GK .circle:nth-child(8) .inner
+  	background: #862D78
+  .GK .circle:nth-child(9) .inner
+  	background: #783084
+  .GK .circle:nth-child(10) .inner
+  	background: #653393
+
+  .AK .circle:nth-child(1) .inner
+  	background: #653594
+  .AK .circle:nth-child(2) .inner
+  	background: #6557A9
+  .AK .circle:nth-child(3) .inner
+  	background: #6672BA
+  .AK .circle:nth-child(4) .inner
+  	background: #6690CC
+  .AK .circle:nth-child(5) .inner
+  	background: #66A9DC
+  .AK .circle:nth-child(6) .inner
+  	background: #5AAFCF
+  .AK .circle:nth-child(7) .inner
+  	background: #479DAD
+  .AK .circle:nth-child(8) .inner
+  	background: #308881
+  .AK .circle:nth-child(9) .inner
+  	background: #207963
+  .AK .circle:nth-child(10) .inner
+  	background: #0B663D
+
+  .AK1 .circle:nth-child(1) .inner
+  	background: #653393
+  .AK1 .circle:nth-child(2) .inner
+  	background: #65429C
+  .AK1 .circle:nth-child(3) .inner
+  	background: #6554A7
+  .AK1 .circle:nth-child(4) .inner
+  	background: #6561AF
+  .AK1 .circle:nth-child(5) .inner
+  	background: #6670B9
+  .AK1 .circle:nth-child(6) .inner
+  	background: #657FC2
+  .AK1 .circle:nth-child(7) .inner
+  	background: #668ECB
+  .AK1 .circle:nth-child(8) .inner
+  	background: #669CD4
+  .AK1 .circle:nth-child(9) .inner
+  	background: #66AADD
+  .AK1 .circle:nth-child(10) .inner
+  	background: #66BCE8
+
+  .AK2 .circle:nth-child(1) .inner
+  	background: #47A7D6
+  .AK2 .circle:nth-child(2) .inner
+  	background: #3F9FC2
+  .AK2 .circle:nth-child(3) .inner
+  	background: #3997B1
+  .AK2 .circle:nth-child(4) .inner
+  	background: #3290A0
+  .AK2 .circle:nth-child(5) .inner
+  	background: #2C8990
+  .AK2 .circle:nth-child(6) .inner
+  	background: #25827F
+  .AK2 .circle:nth-child(7) .inner
+  	background: #1F7B6F
+  .AK2 .circle:nth-child(8) .inner
+  	background: #19745F
+  .AK2 .circle:nth-child(9) .inner
+  	background: #126D4F
+  .AK2 .circle:nth-child(10) .inner
+  	background: #0B663D
+
+  #prefect li
+    list-style-type: circle
+</style>
+
+<style lang="sass">
+  .el-tabs--border-card
+    box-shadow: none !important
+  #selfEvaluations .el-tabs__item
+    height: 32px
+    font-size: 12px
+    line-height: 2.6em
+</style>
---
+++ backend/src/views/PrefectPrint_old.vue
@@ -0,0 +1,418 @@
+<template lang="pug">
+  #prefect
+    div(v-if='loading')
+      Loading
+    div(v-else)
+      el-card(shadow='never').prefectstudents
+        div(slot="header").header
+          //- span Meine Vertraunisschüler*innen
+
+          //- el-collapse-item(v-for='student in prefectStudents', :key='student.username', :title='student.username', :name='student.username').student
+            template(slot='title')
+              icon(icon='user')
+              | &nbsp;
+
+          div
+            span.student-name {{ student.firstname }} {{ student.surname }}
+            el-card(shadow='never')
+              br
+              el-row
+                el-col(:span='9')
+                  .notesWrapper
+                    strong Notes
+                    ul.notes
+                      li.note(v-for='note in student.prefectNotes')
+                        | {{ note.text }}&nbsp;
+                        span.small {{ formatDate(note.createdAt) }}
+                el-col(:span='14', :offset='1')
+                  PrefectNotes(:studentId='student.id')
+
+              el-collapse(v-model='activeSubjects', v-if='!loading')
+                el-collapse-item(v-for='subject in student.studiesSubjects', :key='subject.name', :title='subject.name', :name='subject.name').subject
+                  template(slot='title')
+                    icon(icon='docs')
+                    | &nbsp;&nbsp;
+                    | {{ subject.name }}
+                    .levels.right
+                      | {{ studentSubjectLevel(student, subject.id) }}
+                      | &nbsp;&nbsp;&nbsp;
+
+                  el-row
+                    el-col(:span='15', :offset='1')
+                      //- STUDENT NOTE FROM TEACHER
+                      el-card(shadow='never')
+                        .subjectNote(v-if='studentNoteBySubject(student.studentStudentNotes, subject.id)')
+                          p
+                            em „{{ studentNoteBySubject(student.studentStudentNotes, subject.id).text }}“
+                          p
+                            |– {{ studentNoteBySubject(student.studentStudentNotes, subject.id).teacher.firstname }} {{ studentNoteBySubject(student.studentStudentNotes, subject.id).teacher.surname }}&nbsp;
+                            span.small {{ formatDate(studentNoteBySubject(student.studentStudentNotes, subject.id).updatedAt) }}
+                        .else(v-else)
+                          p No teacher note
+
+                      //- Kompetenzen
+                      .selfEvals(v-if="subject.id !== 'cjlw89ql20imy0149cdcs3ze0'")
+                        //- if not space-station
+                        StudentSelfEvaluations(:subjectId='subject.id', :studentId='student.id')
+
+
+                    el-col(:span='6', :offset='2')
+                      strong Projekten
+                      .projects
+                        .project(v-for='(project, index) in projectsPerSubject(student.studiesProjects, subject.id)', :key='project.slug')
+                          icon(icon='doc')
+                          | {{ project.name }}
+
+              //- CHART
+              el-row
+                el-col(:span='18', :offset='2')
+                  Chart(:selfEvals='student.selfEvaluations', :subjects='student.studiesSubjects', :allSubjects='allSubjects').Chart
+</template>
+
+
+<script>
+import gql from 'graphql-tag'
+import Chart from '@/components/Chart'
+
+export default {
+  name: 'prefectprint',
+  components: {
+    Chart
+  },
+  data () {
+    return {
+      loading: true,
+      dialogLoading: false,
+      dialogVisible: false,
+      activeStudents: [],
+      activeSubjects: [],
+      prefectStudents: [],
+      studentStates: [],
+      activeElCollapse: []
+    }
+  },
+  created () {
+    console.log(this.$route.params.student)
+  },
+  computed: {
+    studentId () {
+      return this.$route.params.student
+    },
+    userId () {
+      return localStorage.getItem('userId')
+    },
+    // student () {
+    //   const studentUsername = this.$route.params.student
+    //   return false
+    // }
+  },
+  methods: {
+    formatDate(date) {
+      const d = new Date(date)
+      return '(' + d.toLocaleDateString("de-DE") + ')'
+    },
+    projectsPerSubject(projects, subjectId) {
+      return projects.filter(o => o.subject.id === subjectId)
+    },
+    studentNoteBySubject(notes, subjectId) {
+      return notes.find(o => o.subject.id === subjectId)
+    },
+    studentSubjectLevel(student, subjectId) {
+      let level = student.studentLevels.find(o => o.subject.id === subjectId)
+      if (!level) return 'BK'
+      else return level.level
+    },
+    addStudent (student) {
+      this.prefectStudents.push(student)
+      this.apolloAddPrefect(student)
+    },
+    removeStudent (student) {
+      this.prefectStudents = this.prefectStudents.filter(o => o.id !== student.id)
+      this.apolloRemovePrefect(student)
+    },
+    isAvailable (student) {
+      let toReturn = true
+      this.prefectStudents.forEach(ps => {
+        if (student.id === ps.id) {
+          toReturn = false
+        }
+      })
+      return toReturn
+    },
+
+    apolloAddPrefect (student) {
+      this.dialogLoading = false
+      this.$apollo.mutate({
+        mutation: gql`mutation($id1: ID!, $id2: ID!) {
+          addToPrefectLinks(
+            prefectLinks1UserId: $id1
+            prefectLinks2UserId: $id2
+          ) {
+            prefectLinks1User {
+              id
+              username
+            }
+            prefectLinks2User {
+              id
+              username
+            }
+          }
+        }`,
+        variables: {
+          id1: student.id,
+          id2: this.userId
+        }
+      })
+      .then((data) => {
+        console.log(data)
+        this.$message({
+          type: 'success',
+          message: 'Student added'
+        })
+        this.dialogLoading = false
+        this.$apolloProvider.defaultClient.reFetchObservableQueries()
+      })
+      .catch((error) => {
+        console.error(error)
+        this.$message({
+          type: 'error',
+          message: error.message
+        })
+        this.dialogLoading = false
+      })
+    },
+
+    apolloRemovePrefect (student) {
+      this.dialogLoading = false
+      this.$apollo.mutate({
+        mutation: gql`mutation($id1: ID!, $id2: ID!) {
+          removeFromPrefectLinks(
+            prefectLinks1UserId: $id1
+            prefectLinks2UserId: $id2
+          ) {
+            prefectLinks1User {
+              id
+              username
+            }
+            prefectLinks2User {
+              id
+              username
+            }
+          }
+        }`,
+        variables: {
+          id1: student.id,
+          id2: this.userId
+        }
+      })
+      .then((data) => {
+        console.log(data)
+        this.$message({
+          type: 'success',
+          message: 'Student removed'
+        })
+        this.dialogLoading = false
+        this.$apolloProvider.defaultClient.reFetchObservableQueries()
+      })
+      .catch((error) => {
+        console.error(error)
+        this.$message({
+          type: 'error',
+          message: error.message
+        })
+        this.dialogLoading = false
+      })
+    }
+  },
+  apollo: {
+    allSubjects: {
+      query: gql`query {
+        allSubjects {
+          id
+          slug
+          name
+          levels
+          competences {
+            id
+            name
+          }
+        }
+      }`,
+      update (result) {
+        // console.log(result.allSubjects)
+        return result.allSubjects
+      }
+    },
+    student: {
+      query: gql`query ($studentId: ID!) {
+      	User(id: $studentId) {
+          id
+      		username
+          firstname
+          surname
+          prefectLinks {
+            id
+            username
+          }
+          studiesSubjects {
+          	id
+          	name
+            slug
+            competences {
+              id
+              name
+            }
+            levels
+          }
+          studiesProjects {
+          	id
+          	name
+          	slug
+            subject {
+              id
+            }
+          }
+          studentLevels {
+            id
+            subject {
+              id
+              slug
+            }
+            level
+          }
+          prefectNotes {
+            id
+            createdAt
+            text
+          }
+          studentStudentNotes {
+            id
+            text
+            subject {
+              id
+              slug
+            }
+            teacher {
+              id
+              username
+              firstname
+              surname
+            }
+            updatedAt
+          }
+          selfEvaluations {
+            value
+            subject {
+              id
+              slug
+              name
+            }
+            student {
+              id
+              username
+            }
+            level
+          }
+      	}
+      }`,
+      variables () {
+        return {
+          studentId: this.$route.params.student
+        }
+      },
+      update (result) {
+        // let prefectStudents = []
+        // result.allUsers.forEach(student => {
+        //   student.prefectLinks.forEach(prefect => {
+        //     if (prefect.id === this.userId) {
+        //       prefectStudents.push(student)
+        //     }
+        //   })
+        // })
+        // this.prefectStudents = prefectStudents
+        //
+        // result.allUsers.forEach(s => {
+        //   if(this.isAvailable(s)) this.studentStates[s.id] = false
+        //   else this.studentStates[s.id] = true
+        // })
+        //
+
+        // return result.allUsers
+        console.log(result.User)
+        this.loading = false
+        return result.User
+      }
+    }
+  },
+}
+</script>
+
+<style lang="sass">
+#prefect
+  .el-collapse-item__header
+    .sli-svg
+      transform: scale(0.8) translate(0px, 6px)
+
+  .el-card__body
+    padding-top: 0 !important
+
+  .userRow:first-child
+    border-top: 1px #EEE solid
+  .userRow
+    border-bottom: 1px #EEE solid
+    .student
+      padding: 10px 0
+      .i
+        margin-right: 8px
+        width: 16px
+        height: 16px
+        transform: translate(0, 2px)
+    .buttons
+      padding-top: 6px
+      button
+        float: right
+
+  .header
+    font-weight: bold
+    text-transform: uppercase
+    font-size: 0.75em
+    letter-spacing: 0.03em
+  .subject
+    .el-collapse-item__header
+      font-size: 1em
+      font-weight: normal
+
+  .level, .levels
+    font-size: 0.86em
+  .small
+    font-size: 0.85em
+  .notesWrapper
+    margin-top: -10px
+  ul, li
+    margin: 0
+    padding: 0
+  ul
+    margin-left: 1.25em
+  ul.notes
+    margin-bottom: 2em
+  .projects
+    margin: 3px 0 0 3px
+  .project
+    padding: 3px 0
+    svg
+      width: 16px
+      height: 16px
+      transform: translate(-4px, 2.5px)
+  .selfEvals
+    margin-top: 1em
+  .Chart
+    margin-top: 2em
+
+@media print 
+  .student-name
+    font-size: 1.5em
+  .el-aside, .el-text-area
+    display: none
+  .el-card
+    border: none
+</style>
